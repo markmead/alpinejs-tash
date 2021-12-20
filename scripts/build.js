6e7e40e56050da5e38d113063993cbecc5d2127f
markmead <itsmarkmead@gmail.com>
2021-12-20 17:11:53 +0000

    update(code clean up)

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,6 @@
-let fs = require("fs");
-
 build({
   entryPoints: [`builds/cdn.js`],
   outfile: `dist/tash.min.js`,
-  bundle: true,
-  minify: true,
-  sourcemap: false,
   platform: "browser",
   define: { CDN: true },
 });
@@ -13,8 +8,6 @@ build({
 build({
   entryPoints: [`builds/module.js`],
   outfile: `dist/tash.esm.js`,
-  bundle: true,
-  bundle: true,
   platform: "neutral",
   mainFields: ["main", "module"],
 });
@@ -22,21 +15,14 @@ build({
 build({
   entryPoints: [`builds/module.js`],
   outfile: `dist/tash.cjs.js`,
-  bundle: true,
   target: ["node10.4"],
   platform: "node",
 });
 
 function build(options) {
   options.define || (options.define = {});
-  options.define["process.env.NODE_ENV"] = process.argv.includes("--watch")
-    ? `'production'`
-    : `'development'`;
 
   return require("esbuild")
-    .build({
-      watch: process.argv.includes("--watch"),
-      ...options,
-    })
+    .build({ ...options, minify: true, bundle: true })
     .catch(() => process.exit(1));
 }
