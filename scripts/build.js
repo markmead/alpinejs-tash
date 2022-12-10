24ca6a30304df1b3701e8dc019536fd08251c671
Mark Mead <itsmarkmead@gmail.com>
2022-12-10 10:38:29 +0000

    Update qoutes

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,11 @@
 buildPlugin({
-  entryPoints: [`builds/cdn.js`],
+  entryPoints: ['builds/cdn.js'],
   outfile: 'dist/tash.min.js',
 })
 
 buildPlugin({
-  entryPoints: [`builds/module.js`],
-  outfile: `dist/tash.esm.js`,
+  entryPoints: ['builds/module.js'],
+  outfile: 'dist/tash.esm.js',
   platform: 'neutral',
   mainFields: ['main', 'module'],
 })
